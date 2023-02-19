11263bc512959d9c5e2038f90098d66d57cf3f4f
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-02-19 14:50:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
-import Preloader from './components/Preloader';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Footer from './components/Footer';
@@ -14,7 +13,6 @@ function App() {
   AOS.init();
   return (
     <>
-    <Preloader/>
     <Namesection/>
     <About/>
     <Skills/> 
